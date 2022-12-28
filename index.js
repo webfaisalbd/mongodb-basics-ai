4e8a5417b97da449f5479faa45b04cf4bd87fcb8
webfaisalbd <web.faisal.bd@gmail.com>
2022-12-28 22:55:26 +0600

    mongodb connected with server using async wait

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ const app = express();
 
 const PORT = 4537;
 
-mongoose.connect('mongodb://localhost:27017/testProductDB')
-.then(()=> console.log("MongoDb is connected."))
-.catch((error)=> {
-    console.log("MongoDb is not connected.");
-    console.log(error);
-    process.exit(1)
-} )
+// mongoose.connect('mongodb://localhost:27017/testProductDB')
+// .then(()=> console.log("MongoDb is connected."))
+// .catch((error)=> {
+//     console.log("MongoDb is not connected.");
+//     console.log(error);
+//     process.exit(1)
+// } )
 
-app.get('/', (req,res)=> {
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/testProductDB')
+        console.log("db is connected.")
+    } catch (error) {
+        console.log('db is not connected');
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
+app.get('/', (req, res) => {
     res.send("<h2>Welcome to local host</h2>");
 })
 
-app.listen(PORT, ()=> {
+app.listen(PORT, async () => {
     console.log(`Server is running at http://localhost:${PORT}`);
+    await connectDB();
 })
