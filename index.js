322ce60163ee296307d201d7f9fe58cd355bcf1f
webfaisalbd <web.faisal.bd@gmail.com>
2022-12-28 22:48:20 +0600

    mongodb connected with server using then catch

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = 4537;
+
+mongoose.connect('mongodb://localhost:27017/testProductDB')
+.then(()=> console.log("MongoDb is connected."))
+.catch((error)=> {
+    console.log("MongoDb is not connected.");
+    console.log(error);
+    process.exit(1)
+} )
 
 app.get('/', (req,res)=> {
     res.send("<h2>Welcome to local host</h2>");
