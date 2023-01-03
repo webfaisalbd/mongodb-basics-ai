f2802a3cd6f13a1da16771a6ec43e153d59524e6
webfaisalbd <web.faisal.bd@gmail.com>
2023-01-04 00:57:03 +0600

    post data, get data, get a specific data using params recap

--- index.js
+++ index.js
@@ -1,30 +1,98 @@
+const { urlencoded } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
-
 const PORT = 4000;
 
-const mongodbConnect = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/schoolManagementSystem')
-        .then(() => console.log("mongodb is connected."))
-        .catch(error => {
-            console.log(error);
-            console.log("mongodb is not connected.")
-        })
+const app = express();
+app.use(express.json());
+
+const mongodbConnected = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/schoolManagement')
+        .then(() => console.log("mongodb connected"))
+        .catch(error => console.log("mongodb is not connected"))
 }
 
+// make schema 
+const schoolManagementSchema = new mongoose.Schema({
+    studentClassName: {
+        type: String,
+        required: true
+    },
+    studentNumber: {
+        type: Number,
+        required: true,
+        max: [40, "maximum number of student 40 in a class"],
+        min: [10, "minimum number of student 10 in a class"]
+    },
+    studentSubject: {
+        type: String,
+        required: true
+    },
+    studentGolden: {
+        type: Number,
+        required: true
+    }
 
-// make schema
+})
 
-// make model 
+// make model
+const SchoolManagementModel = mongoose.model("students", schoolManagementSchema);
 
-// get 
-app.get('/', (req,res)=> {
-    res.send('Welcome to localhost.')
+
+
+// get students
+app.get("/students", async (req, res) => {
+    try {
+        const students = await SchoolManagementModel.find();
+        if (students) {
+            res.status(201).send(students);
+        } else {
+            res.status(404).send("students not found.");
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+})
+
+// get specific students
+app.get('/students/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const student = await SchoolManagementModel.find({ _id: id }).select({ _id: 0 })
+        if (student) {
+            res.status(201).send(student);
+        } else {
+            res.status(404).send("student not found.");
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+})
+
+// post students
+app.post('/students', async (req, res) => {
+    try {
+        const newData = new SchoolManagementModel({
+            studentClassName: req.body.studentClassName,
+            studentNumber: req.body.studentNumber,
+            studentSubject: req.body.studentSubject,
+            studentGolden: req.body.studentGolden
+        })
+        const students = await newData.save();
+        res.status(201).send(students);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+})
+
+// get
+app.get('/', (req, res) => {
+    res.send("Welcome to localhost")
 })
 
 app.listen(PORT, async () => {
-    console.log(`The server is running at http://localhost:${PORT}`);
-    await mongodbConnect();
+    console.log(`server is connected at http://localhost:${PORT}`);
+    await mongodbConnected();
 })
